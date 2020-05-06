59a26cda7435034cf07162a7743acff0382355c3
ararat1393 <54005752+ararat1393@users.noreply.github.com>
2020-05-06 13:08:54 +0400

    remove an HTML element using the DOMDocument class

---
+++ remove_html.php
@@ -0,0 +1,22 @@
+    /**
+     * Will use DOMDocument to load html content
+     * Will use DomXPath to find the div element with class name
+     * @param $text
+     * @param $className
+     * @return bool
+     */
+    public function removeDivByClassName(&$html , $className )
+    {
+        $dom = new \DOMDocument();
+        $dom->preserveWhiteSpace = false;
+        $dom->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
+
+        $xpath = new \DomXPath($dom);
+        $xpathResults = $xpath->query("//div[contains(@class, '$className')]");
+
+        if($el = $xpathResults->item(0)){
+            $el->parentNode->removeChild($el);
+            $html =  $dom->saveHTML();
+        }
+        return true;
+    }
